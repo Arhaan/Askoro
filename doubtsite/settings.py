a4bfe4776c2bd8fc6d05c4731f4c9db567151dff
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2020-05-13 17:26:27 +0530

    Set debug value

--- doubtsite/settings.py
+++ doubtsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DOUBTSITE_SECRET_KEY', 'aslvjbaskfbweuofbb1234rqwer')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
 ALLOWED_HOSTS = ["askoro.herokuapp.com"]
 
