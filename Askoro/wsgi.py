d5ee3c31fa26377c6cfcbb30c0eead0a36a7fcd2
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2020-08-10 20:12:10 +0530

    Corrected the errors related to changing the name

--- Askoro/wsgi.py
+++ Askoro/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doubtsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Askoro.settings')
 
 application = get_wsgi_application()
